c05382712e3e4a1092aa19fceef2ef08fa5b947b
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2023-02-28 14:05:29 +0500

    fix: UI Issuus On Server

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -59,77 +59,74 @@ export default function PrimarySearchAppBar() {
             shadow-2xl shadow-[#499EB6] md:shadow-2xl md:shadow-[#A4D8DD]  ' > */}
                 <AppBar position="static" className=' bg-gradient-to-r from-[#BDD9DD] via-[#e2dff5] to-[#b2e4e9]
             shadow-2xl shadow-[#499EB6] md:shadow-2xl md:shadow-[#A4D8DD] appearance-none ' >
-
-                    <Toolbar>
-                        <Box className='rotate-[-7.12deg] mt-[50px] bg-[#FFDEA7] mb-[50px]
+                    <Container>
+                        <Toolbar>
+                            <Box className='rotate-[-7.12deg] mt-[50px] bg-[#FFDEA7] mb-[50px]
                          h-[53px] w-[128px] text-center mr-[-5px]'>
-                            <Typography
-                                variant='h3'
-                                gutterBottom
-                                component='div'
-                                className='text-60px font-normal text-[#000000]
+                                <Typography
+                                    variant='h3'
+                                    gutterBottom
+                                    component='div'
+                                    className='text-60px font-normal text-[#000000]
                                      font-[Libre Baskerville] sticky rotate-[7.12deg] cursor-pointer '
-                            >
-                                treas
-                            </Typography>
-                        </Box>
-                        <Box width={600}>
-                            <Search sx={{ height: "40px" }} className='flex
+                                >
+                                    treas
+                                </Typography>
+                            </Box>
+                            <Box width={600}>
+                                <Search sx={{ height: "40px" }} className='flex
                             appearance-none rounded-[15px] bg-[#ffffff38] '>
-                                <SearchIconWrapper className=' appearance-none'>
-                                    <Image src="image/image 118.svg" alt='' width={20} height={20} />
-                                </SearchIconWrapper>
-                                <StyledInputBase
-                                    sx={{ color: "#3B3A40", fontWeight: "700" }}
-                                    placeholder="Scan any treas ETH wallet address or ENS name"
-                                    inputProps={{ 'aria-label': 'search' }}
-                                    fullWidth
+                                    <SearchIconWrapper className=' appearance-none'>
+                                        <Image src="image/image 118.svg" alt='' width={20} height={20} />
+                                    </SearchIconWrapper>
+                                    <StyledInputBase
+                                        sx={{ color: "#3B3A40", fontWeight: "700" }}
+                                        placeholder="Scan any treas ETH wallet address or ENS name"
+                                        inputProps={{ 'aria-label': 'search' }}
+                                        fullWidth
 
-                                />
-                                <Image src="image/image 131.svg" alt="" width={30} height={30} className="cursor-pointer mr-[15px]" />
-                            </Search>
-                        </Box>
-                        <Box />
-                        <Box display="flex">
-
-
-                            <Button sx={{
-                                fontSize: "16px",
-                                color: "#000000", borderColor: "#8E8C95",
-                                borderBlockColor: "#8E8C95", borderRadius: "rounded", marginRight: "10px",
-                                fontWeight: "700"
-                            }}
-                            >Learn</Button>
+                                    />
+                                    <Image src="image/image 131.svg" alt="" width={30} height={30} className="cursor-pointer mr-[15px]" />
+                                </Search>
+                            </Box>
+                            <Box />
+                            <Box display="flex">
 
 
+                                <Button sx={{
+                                    fontSize: "16px",
+                                    color: "#000000", borderColor: "#8E8C95",
+                                    borderBlockColor: "#8E8C95", borderRadius: "rounded", marginRight: "10px",
+                                    fontWeight: "700"
+                                }}
+                                >Learn</Button>
 
-                            <Button sx={{
-                                fontSize: "16px",
-                                color: "#000000", borderColor: "#8E8C95",
-                                borderBlockColor: "#8E8C95", borderRadius: "rounded", marginRight: "10px",
-                                fontWeight: "700"
 
-                            }}
-                                className='text-[#000000] 
+
+                                <Button sx={{
+                                    fontSize: "16px",
+                                    color: "#000000", borderColor: "#8E8C95",
+                                    borderBlockColor: "#8E8C95", borderRadius: "rounded", marginRight: "10px",
+                                    fontWeight: "700"
+
+                                }}
+                                    className='text-[#000000] 
                                 text-[16px] font-bold  border-[#8E8C95] mr-[10px] '
-                            >Solutions</Button>
+                                >Solutions</Button>
 
+                                <Button sx={{
+                                    fontSize: "16px", backgroundColor: "white",
+                                    color: "#000000", borderColor: "#8E8C95", borderStyle: "solid",
+                                    borderBlockColor: "#8E8C95]", borderRadius: "rounded", marginRight: "10px",
+                                    paddingX: "15px", fontWeight: "700"
+                                }}
+                                >Access Treas</Button>
 
+                            </Box>
 
-                            <Button sx={{
-                                fontSize: "16px", backgroundColor: "white",
-                                color: "#000000", borderColor: "#8E8C95", borderStyle: "solid",
-                                borderBlockColor: "#8E8C95]", borderRadius: "rounded", marginRight: "10px",
-                                paddingX: "15px", fontWeight: "700"
-                            }}
-                            >Access Treas</Button>
+                        </Toolbar >
 
-
-                        </Box>
-
-                    </Toolbar >
-
-
+                    </Container>
                 </AppBar >
                 {/* <Typography sx={{ border: "none" }} width="100%"
                     className=' bg-gradient-to-r from-[#c7e3e6] via-[#e2dff5] to-[#b2e4e9]
